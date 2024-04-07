634e0e20ea0dbe76e8f6fa0f4487bd2730ec02c3
Miala <65893041+Miala-python@users.noreply.github.com>
2024-04-07 19:05:12 +0200

    +ping

--- main.ts
+++ main.ts
@@ -2,18 +2,44 @@ function someone () {
     if (!(music.isSoundPlaying())) {
         music.play(music.stringPlayable("E B E A E G E F ", 200), music.PlaybackMode.LoopingInBackground)
     }
-    if (input.buttonIsPressed(Button.AB)) {
-        music.stopAllSounds()
-        basic.pause(10000)
-    }
 }
 // 8 => PIR Motion Sensor
+// 1; 2 => Ultrasonic Module
+let son_à_stopper_ = false
+let loaded = false
 led.enable(false)
 pins.digitalWritePin(DigitalPin.P8, 0)
+music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.UntilDone)
+basic.pause(2000)
+music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
+loaded = true
+let dist_OK = sonar.ping(
+DigitalPin.P2,
+DigitalPin.P1,
+PingUnit.Centimeters
+)
 basic.forever(function () {
-    if (1 == pins.digitalReadPin(DigitalPin.P8)) {
-        someone()
-    } else {
+    if (loaded) {
+        if (1 == pins.digitalReadPin(DigitalPin.P8)) {
+            someone()
+        } else {
+            if (10 < Math.abs(dist_OK - sonar.ping(
+            DigitalPin.P2,
+            DigitalPin.P1,
+            PingUnit.Centimeters
+            ))) {
+                someone()
+            }
+        }
+        if (input.buttonIsPressed(Button.AB)) {
+            music.stopAllSounds()
+            basic.pause(10000)
+        }
+    }
+})
+loops.everyInterval(7000, function () {
+    if (son_à_stopper_) {
         music.stopAllSounds()
     }
+    son_à_stopper_ = music.isSoundPlaying()
 })
